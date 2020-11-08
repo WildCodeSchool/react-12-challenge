9a5d854518237955152a2cefdb1e3c42c5a9b47b
loulou <yourdj.france@gmail.com>
2020-11-08 15:11:30 +0100

    challenge resolved

--- src/TabSelector.js
+++ src/TabSelector.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
+const Home = () => <p>This is the Home component!</p>;
+const About = () => <p>This challenge is about tabs!</p>;
+const Contact = () => <p>Feel free to contact us!</p>;
+const Error = () => <p>Something went <strong>wrong</strong>!</p>;
+
+
 class TabSelector extends Component {
   constructor(props) {
     super(props);
@@ -14,9 +20,23 @@ class TabSelector extends Component {
     this.setState({ activeId: buttonId });
   }
 
+  getTabContent() {
+    switch(this.state.activeId) {
+      case 'home':
+        return <Home />;
+      case 'about':
+        return <About />;  
+      case 'contact':
+        return <Contact />;  
+      default:
+        return <Error />;
+    }
+  }
+
   render() {
     const { activeId } = this.state;
     return (
+      <>
       <div className="TabSelector">
         <button
           id="home"
@@ -39,7 +59,14 @@ class TabSelector extends Component {
         >
           Contact
         </button>
+        
       </div>
+      <div className="App-content">
+      {
+        this.getTabContent()
+      }
+    </div>
+    </>
     );
   }
 }
