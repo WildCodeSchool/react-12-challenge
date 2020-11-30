627598984585b41e77877f5293ff774738e00dc5
GuilhermeC18 <52559295+GuilhermeC18@users.noreply.github.com>
2020-11-30 11:54:23 +0100

    12-solved

--- src/App.js
+++ src/App.js
@@ -2,37 +2,14 @@ import React, { Component } from 'react';
 import TabSelector from './TabSelector';
 import './App.css';
 
-const Home = () => <p>This is the Home component!</p>;
-const About = () => <p>This challenge is about tabs!</p>;
-const Contact = () => <p>Feel free to contact us!</p>;
-const Error = () => <p>Something went <strong>wrong</strong>!</p>;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeId: 'home'
-    };
-  }
-
-  getTabContent() {
-    switch(this.state.activeId) {
-      case 'home':
-        return <Home />;
-      default:
-        return <Error />;
-    }
-  }
+  
 
   render() {
     return (
       <div className="App">
         <TabSelector />
-        <div className="App-content">
-          {
-            this.getTabContent()
-          }
-        </div>
       </div>
     );
   }
